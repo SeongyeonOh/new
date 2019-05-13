0369fdcdad274a6ba94febe9738a08bb3afc79ba
SeongyeonOh <songyon95@gmail.com>
2019-05-13 22:40:26 +0900

    Add cal.py

---
+++ cal.py
@@ -0,0 +1,16 @@
+from plus import plus
+from minus import minus
+from multifly import multifly
+from divide import divide
+
+
+a = input("Enter your number : ")
+a = int(a)
+b = input("Enter your number : ")
+b = int(b)
+
+print("=========")
+plus(a, b)
+minus(a, b)
+multifly(a, b)
+divide(a, b)
